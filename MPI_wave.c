656f9602fbab9f57ea1646c492d63db327d4dfc4
Nicolas Champdavoine <nich9513@rackham2.uppmax.uu.se>
2017-05-10 16:06:16 +0200

    Code that work well

--- MPI_wave.c
+++ MPI_wave.c
@@ -7,8 +7,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define WRITE_TO_FILE 
-#define VERIFY 1 
+#define VERIFY 
 
 void Prvalues(int length, int heigth,  double matrix[length * heigth]);
 double timer();
@@ -36,7 +35,7 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    Nx=128;
+    //reading inputs
     Nx=atoi(argv[1]);
     p1=atoi(argv[2]);
     p2=atoi(argv[3]);
@@ -47,30 +46,30 @@ int main(int argc, char *argv[]){
     dt=0.50*dx;
     lambda_sq = (dt/dx)*(dt/dx);
 
-    int block_length = (Ny - 2)/p2 + 2; 
-    int block_heigth = (Nx- 2)/p1 + 2;
 
-    int mod_length = (Ny - 2)%p2; 
-    int mod_heigth = (Nx - 2)%p1; 
-    if(mod_length != 0){
-        block_length += 1;
-        mod_length = p2 - mod_length;
-    }
+    //calcul of the length and heigth of a block even if N non divisible
+    int block_heigth = (Ny - 2)/p2 + 2; 
+    int block_length = (Nx- 2)/p1 + 2;
 
+    int mod_heigth = (Ny - 2)%p2; 
+    int mod_length = (Nx - 2)%p1; 
     if(mod_heigth != 0){
         block_heigth += 1;
-        mod_heigth = p1 - mod_heigth;
+        mod_heigth = p2 - mod_heigth;
     }
 
+    if(mod_length != 0){
+        block_length += 1;
+        mod_length = p1 - mod_length;
+    }
 
-    printf("len %i hei %i\n", block_length, block_heigth);
 
     MPI_Request req_u_send;
     MPI_Request req_u_recv;
     MPI_Request req_u_new_send;
     MPI_Request req_u_new_recv;
 
-    MPI_Request req_shift_left[2];
+    MPI_Request req_shift[2];
 
 
 
@@ -127,27 +126,27 @@ int main(int argc, char *argv[]){
     /* Define new type to send datas */
 	MPI_Datatype blocktype, blockselect, rowtype, coltype;
 
-	MPI_Type_contiguous(block_length * block_heigth, MPI_DOUBLE, &blocktype);
+	MPI_Type_contiguous(block_heigth * block_length, MPI_DOUBLE, &blocktype);
     MPI_Type_commit(&blocktype);
 
-	MPI_Type_vector(block_length, block_heigth, Nx, MPI_DOUBLE, &blockselect);
+	MPI_Type_vector(block_heigth, block_length, Nx, MPI_DOUBLE, &blockselect);
 	MPI_Type_commit(&blockselect);
 
-    MPI_Type_contiguous(block_heigth, MPI_DOUBLE, &rowtype);
+    MPI_Type_contiguous(block_length, MPI_DOUBLE, &rowtype);
     MPI_Type_commit(&rowtype);
 
-	MPI_Type_vector(block_length, 1, block_heigth, MPI_DOUBLE, &coltype);
+	MPI_Type_vector(block_heigth, 1, block_length, MPI_DOUBLE, &coltype);
 	MPI_Type_commit(&coltype);
 
+    //Set mod to 0 in all procs except those on the last column/row
     if(row_rank != p1-1 ){
-        mod_heigth = 0;
-    }
-
-    if(col_rank != p2-1 ){
         mod_length = 0;
     }
 
-    printf("coords %i %i rank %i row %i\n ", coords[0], coords[1], rank, row_rank);
+    if(col_rank != p2-1 ){
+        mod_heigth = 0;
+    }
+
     if(rank == 0){
         u = malloc(Nx*Ny*sizeof(double));
         u_old = malloc(Nx*Ny*sizeof(double));
@@ -175,7 +174,6 @@ int main(int argc, char *argv[]){
         }
 
 
-        Prvalues(Nx, Ny, u);
         /* Send all parts of u_old, u and u_new to the right processors */
         for(int i = 0; i < p1; i++) {
             for(int j = 0; j < p2; j++) {
@@ -184,22 +182,27 @@ int main(int argc, char *argv[]){
                 temp_coords[1] = j ;
 
                 MPI_Cart_rank(grid_comm,temp_coords,&temp_rank);
-                printf("send to %i %i %i\n", i, j, temp_rank);
 
-                MPI_Isend(&u[i*(block_heigth-2) + j*(block_length-2)*Nx], 1, blockselect, temp_rank, 0, grid_comm, &req_u_send);
-                MPI_Isend(&u_new[i*(block_heigth-2) + j*(block_length-2)*Nx], 1, blockselect, temp_rank, 1, grid_comm, &req_u_new_send);
+                MPI_Isend(&u[i*(block_length-2) + j*(block_heigth-2)*Nx], 1, blockselect, temp_rank, 0, grid_comm, &req_u_send);
+                MPI_Isend(&u_new[i*(block_length-2) + j*(block_heigth-2)*Nx], 1, blockselect, temp_rank, 1, grid_comm, &req_u_new_send);
 
             }
         } 
     }
 
+#ifdef VERIFY
+        double max_error=0.0;
+        double local_max_error = 0.0;
+#endif
+
+
 
     /* Each processor receive block of u0 and u1 as initialization */
-    u_blocks = (double *)malloc(block_length * block_heigth * sizeof(double));
-    u_new_blocks = (double *)malloc(block_length * block_heigth * sizeof(double));
-    u_old_blocks = (double *)malloc(block_length * block_heigth * sizeof(double));
+    u_blocks = (double *)malloc(block_heigth * block_length * sizeof(double));
+    u_new_blocks = (double *)malloc(block_heigth * block_length * sizeof(double));
+    u_old_blocks = (double *)malloc(block_heigth * block_length * sizeof(double));
 
-    memset(u_old_blocks,1,block_length*block_heigth*sizeof(double));
+    memset(u_old_blocks,1,block_heigth*block_length*sizeof(double));
 
     MPI_Irecv(u_blocks, 1, blocktype, 0, 0, grid_comm, &req_u_recv);
     MPI_Irecv(u_new_blocks, 1, blocktype, 0, 1, grid_comm, &req_u_new_recv);
@@ -209,17 +212,15 @@ int main(int argc, char *argv[]){
 
 
     /* Printing part */
-    printf("coords %i %i\n", row_rank, col_rank);
-//    Prvalues(block_heigth, block_length, u_blocks);
-//    Prvalues(block_heigth, block_length, u_new_blocks);
+//    printf("coords %i %i\n", row_rank, col_rank);
+//    Prvalues(block_length, block_heigth, u_blocks);
+//    Prvalues(block_length, block_heigth, u_new_blocks);
   
-    /*
-    each processors receive its right part of u_old, u, u_new from the root processor (plus the halo points already?) and store them in variables called u_old_local, u_local, u_new_local 
-    */
-
     /* integration of the solution on each processors */
 
-    for(int n=2; n<Nt; n++) {
+    begin=MPI_Wtime();
+
+    for(int n=2; n<Nt; ++n) {
         // Swap ptrs 
         double *tmp_blocks = u_old_blocks;
         u_old_blocks = u_blocks;
@@ -227,56 +228,63 @@ int main(int argc, char *argv[]){
         u_new_blocks = tmp_blocks;
 
         // Apply stencil
-        for(int i = 1; i < (block_length-1-mod_length); i++) {
-            for(int j = 1; j < (block_heigth-1-mod_heigth); j++) {
-                u_new_blocks[i*block_heigth+j] = 2*u_blocks[i*block_heigth+j] - u_old_blocks[i*block_heigth+j] + lambda_sq*(u_blocks[(i+1)*block_heigth+j] + u_blocks[(i-1)*block_heigth+j] + u_blocks[i*block_heigth+j+1] + u_blocks[i*block_heigth+j-1] - 4*u_blocks[i*block_heigth+j]);
+        for(int i = 1; i < (block_heigth-1-mod_heigth); i++) {
+            for(int j = 1; j < (block_length-1-mod_length); j++) {
+                u_new_blocks[i*block_length+j] = 2*u_blocks[i*block_length+j] - u_old_blocks[i*block_length+j] + lambda_sq*(u_blocks[(i+1)*block_length+j] + u_blocks[(i-1)*block_length+j] + u_blocks[i*block_length+j+1] + u_blocks[i*block_length+j-1] - 4*u_blocks[i*block_length+j]);
         
+
+#ifdef VERIFY
+                double e = fabs(u_new_blocks[i*block_length + j] - initialize((j+row_rank*(block_length-2))*dx, (i+col_rank*(block_heigth-2))*dx, n*dt));
+                if(e>local_max_error){
+                      local_max_error = e;
+                }
+#endif
+
             }
         }
-
-    
-        //Prvalues(block_heigth, block_length, u_new_blocks);
         
-        // exchange ghost values
-
+        /* exchange ghost values */
         int source, dest;
         //sending left column
         MPI_Cart_shift(grid_comm, 0, 1, &source, &dest);
-        MPI_Isend(&u_new_blocks[1], 1, coltype, source, 11, grid_comm, &req_shift_left[0]);
-        MPI_Irecv(&u_new_blocks[block_heigth-1], 1, coltype, dest, 11, grid_comm, &req_shift_left[1]);
-        MPI_Waitall(2, req_shift_left, MPI_STATUS_IGNORE);
+        MPI_Isend(&u_new_blocks[1], 1, coltype, source, 11, grid_comm, &req_shift[0]);
+        MPI_Irecv(&u_new_blocks[block_length-1], 1, coltype, dest, 11, grid_comm, &req_shift[1]);
+        MPI_Waitall(2, req_shift, MPI_STATUS_IGNORE);
 
         //sending right column
         MPI_Cart_shift(grid_comm, 0, -1, &source, &dest);
-        MPI_Isend(&u_new_blocks[block_heigth-2], 1, coltype, source, 22, grid_comm, &req_shift_left[0]);
-        MPI_Irecv(&u_new_blocks[0], 1, coltype, dest, 22, grid_comm, &req_shift_left[1]);
-        MPI_Waitall(2, req_shift_left, MPI_STATUS_IGNORE);
+        MPI_Isend(&u_new_blocks[block_length-2], 1, coltype, source, 22, grid_comm, &req_shift[0]);
+        MPI_Irecv(&u_new_blocks[0], 1, coltype, dest, 22, grid_comm, &req_shift[1]);
+        MPI_Waitall(2, req_shift, MPI_STATUS_IGNORE);
 
         //sending bottom row
         MPI_Cart_shift(grid_comm, 1, -1, &source, &dest);
-        MPI_Isend(&u_new_blocks[(block_length - 2)*block_heigth], 1, rowtype, source, 33, grid_comm, &req_shift_left[0]);
-        MPI_Irecv(&u_new_blocks[0], 1, rowtype, dest, 33, grid_comm, &req_shift_left[1]);
-        MPI_Waitall(2, req_shift_left, MPI_STATUS_IGNORE);
+        MPI_Isend(&u_new_blocks[(block_heigth - 2)*block_length], 1, rowtype, source, 33, grid_comm, &req_shift[0]);
+        MPI_Irecv(&u_new_blocks[0], 1, rowtype, dest, 33, grid_comm, &req_shift[1]);
+        MPI_Waitall(2, req_shift, MPI_STATUS_IGNORE);
 
         //sending top row
         MPI_Cart_shift(grid_comm, 1, 1, &source, &dest);
-        MPI_Isend(&u_new_blocks[block_heigth], 1, rowtype, source, 44, grid_comm, &req_shift_left[0]);
-        MPI_Irecv(&u_new_blocks[(block_length-1)*block_heigth], 1, rowtype, dest, 44, grid_comm, &req_shift_left[1]);
-        MPI_Waitall(2, req_shift_left, MPI_STATUS_IGNORE);
-
- //       sleep(rank + 7);
- //       printf("coords %i %i\n", row_rank, col_rank);
- //       Prvalues(block_heigth, block_length, u_new_blocks);
-
-
+        MPI_Isend(&u_new_blocks[block_length], 1, rowtype, source, 44, grid_comm, &req_shift[0]);
+        MPI_Irecv(&u_new_blocks[(block_heigth-1)*block_length], 1, rowtype, dest, 44, grid_comm, &req_shift[1]);
+        MPI_Waitall(2, req_shift, MPI_STATUS_IGNORE);
 
     }
+    end = MPI_Wtime();
+    if(rank == 0){
+        printf("Time : %f\n", end - begin);
+    }
 
-
+#ifdef VERIFY
+    MPI_Reduce(&local_max_error, &max_error, 1, MPI_DOUBLE, MPI_MAX, 0, grid_comm);
+    if(rank == 0){
+        printf("Max error : %f\n", max_error);
+    }
+#endif
   
-        sleep(1+rank);
-        printf("coords %i %i\n", row_rank, col_rank);
-        Prvalues(block_heigth, block_length, u_new_blocks);
+//        sleep(1+rank);
+//        printf("coords %i %i\n", row_rank, col_rank);
+//        Prvalues(block_length, block_heigth, u_new_blocks);
  
 
     MPI_Type_free(&blockselect);
@@ -294,81 +302,6 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-/********************************************************/
-/*This part below is taken from the serial part wave.c */
-/******************************************************/
-
-/*  for(int i = 1; i < (Ny-1); ++i) {
-    for(int j = 1; j < (Nx-1); ++j) {
-      double x = j*dx;
-      double y = i*dx;
-
-      // u0 
-      u[i*Nx+j] = initialize(x,y,0);
-
-      // u1 
-      u_new[i*Nx+j] = initialize(x,y,dt);
-    }
-  }
-
-#ifdef WRITE_TO_FILE
-  save_solution(u_new,Ny,Nx,1);
-#endif
-#ifdef VERIFY
-  double max_error=0.0;
-#endif
-
-  // Integrate 
-
-  begin=timer();
-  for(int n=2; n<Nt; ++n) {
-    // Swap ptrs 
-    double *tmp = u_old;
-    u_old = u;
-    u = u_new;
-    u_new = tmp;
-
-    // Apply stencil 
-    for(int i = 1; i < (Ny-1); ++i) {
-      for(int j = 1; j < (Nx-1); ++j) {
-
-        u_new[i*Nx+j] = 2*u[i*Nx+j]-u_old[i*Nx+j]+lambda_sq*(u[(i+1)*Nx+j] + u[(i-1)*Nx+j] + u[i*Nx+j+1] + u[i*Nx+j-1] -4*u[i*Nx+j]);
-      }
-    }
-
-#ifdef VERIFY
-    double error=0.0;
-    for(int i = 0; i < Ny; ++i) {
-      for(int j = 0; j < Nx; ++j) {
-        double e = fabs(u_new[i*Nx+j]-initialize(j*dx,i*dx,n*dt));
-        if(e>error)
-          error = e;
-      }
-    }
-    if(error > max_error)
-      max_error=error;
-#endif
-
-#ifdef WRITE_TO_FILE
-    save_solution(u_new,Ny,Nx,n);
-#endif
-
-  }
-  end=timer();
-
-  printf("Time elapsed: %g s\n",(end-begin));
-
-#ifdef VERIFY
-  printf("Maximum error: %g\n",max_error);
-#endif
-
-  free(u);
-  free(u_old);
-  free(u_new);
-
-  return 0;
-}
-*/
 
 double timer()
 { 
